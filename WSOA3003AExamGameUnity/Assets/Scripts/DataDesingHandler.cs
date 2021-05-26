0214cd6f256cd589a3073e6a8acc5b60cc4c031b
Alon-mizrahi <alonmiz1@gmail.com>
2021-05-26 17:21:54 +0200

    Added custom objects from blender. created datahandler script

---
+++ WSOA3003AExamGameUnity/Assets/Scripts/DataDesingHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataDesingHandler : MonoBehaviour
+{
+    //This script is used to handle the data design of the game objects
+    //It is used to centralise all data
+    // should be attached to GameManager object, singal instance
+    //
+
+    //eg: ball power handler, maybe object friction as well
+    //
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
